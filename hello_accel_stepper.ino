9d058589831f9ee4b4d170bae4c5ba9f4fc02d52
Benjamin Doerig <b@doerig.io>
2023-01-20 13:19:55 +0100

    movement controlled by quick button press

--- hello_accel_stepper.ino
+++ hello_accel_stepper.ino
@@ -4,22 +4,30 @@
 #define X_DIR     5
 #define X_STP     2
 
+#define FORWARD     9 // limit X
+#define BACKWARD     10 // limit Y
+
 AccelStepper stepper(AccelStepper::DRIVER, X_STP, X_DIR);
 
 void setup()
 {
+  pinMode(FORWARD, INPUT_PULLUP);
+  pinMode(BACKWARD, INPUT_PULLUP);
+
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
 
   stepper.setMaxSpeed(3000); // steps/sec
   stepper.setAcceleration(1000); // steps/sec^2 - Amount of steps the speed is adjusted every sec
-  stepper.moveTo(6400);
 }
 
 void loop()
 {
-    if (stepper.distanceToGo() == 0)
-      stepper.moveTo(-stepper.currentPosition());
-
+  if(!digitalRead(FORWARD)){
+      stepper.moveTo(10000);
+  }
+  if(!digitalRead(BACKWARD)){
+      stepper.moveTo(0);
+  }
     stepper.run();
 }
