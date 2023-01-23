ab589fafe364de86ec4e7cc48802ca6cb38ba0fc
Benjamin Doerig <b@doerig.io>
2023-01-23 20:40:09 +0100

    move motor only while button is pressed

--- hello_accel_stepper.ino
+++ hello_accel_stepper.ino
@@ -7,6 +7,11 @@
 #define FORWARD     9 // limit X
 #define BACKWARD     10 // limit Y
 
+#define MAX_SPEED 2//rotations/s
+#define ROTATION_STEPS 3200 // 400*8
+
+char status = 'S'; // S:Stopped, F:Forward, B:Backward
+
 AccelStepper stepper(AccelStepper::DRIVER, X_STP, X_DIR);
 
 void setup()
@@ -17,17 +22,53 @@ void setup()
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
 
-  stepper.setMaxSpeed(3000); // steps/sec
-  stepper.setAcceleration(1000); // steps/sec^2 - Amount of steps the speed is adjusted every sec
+  stepper.setMaxSpeed(5000); // steps/sec
+  stepper.setAcceleration(4000); // steps/sec^2 - Amount of steps the speed is adjusted every sec
 }
 
 void loop()
 {
-  if(!digitalRead(FORWARD)){
+  switch(status){
+    case 'S':
+      stopped();
+      break;
+    case 'F':
+      forward();
+      break;
+    case 'B':
+      backward();
+      break;
+  }
+}
+
+void stopped(){
+  if(isPressed(FORWARD)){
       stepper.moveTo(10000);
+      status = 'F';
   }
-  if(!digitalRead(BACKWARD)){
+  if(isPressed(BACKWARD)){
       stepper.moveTo(0);
+      status = 'B';
   }
-    stepper.run();
+  stepper.run();
+}
+
+void forward(){
+  if(!isPressed(FORWARD)){
+      stepper.stop();
+      status = 'S';
+  }
+  stepper.run();
+}
+
+void backward(){
+  if(!isPressed(BACKWARD)){
+      stepper.stop();
+      status = 'S';
+  }
+  stepper.run();
+}
+
+bool isPressed(int pin){
+  return !digitalRead(pin);
 }
